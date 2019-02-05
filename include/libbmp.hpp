dd58a91ca2067355616305c8486e42af7b3c8374
foxman <lin.vladimir@gmail.com>
2019-02-05 17:15:35 +0800

    First Simple Version

--- include/libbmp.hpp
+++ include/libbmp.hpp
@@ -1,81 +1,44 @@
 /****************************************************************************\
  *
- *                         SUID and TUID basic functions
+ *                         libbmp
  *
 \****************************************************************************/
 
-#ifndef UUIDS_HPP
-#define UUIDS_HPP
+#ifndef LIBBMP_HPP
+#define LIBBMP_HPP
 
 #if defined (WIN32) && defined(BUILD_SHARED_LIBS)
   #if defined (_MSC_VER)
     #pragma warning(disable: 4251)
   #endif
-  #if defined(EXPORT_UUIDS)
-    #define UUIDs_EXPORT __declspec(dllexport)
+  #if defined(EXPORT_LIBBMP)
+    #define LIBBMP_EXPORT __declspec(dllexport)
   #else
-    #define UUIDs_EXPORT __declspec(dllimport)
+    #define LIBBMP_EXPORT __declspec(dllimport)
   #endif
 #else
-    #define UUIDs_EXPORT
+    #define LIBBMP_EXPORT
 #endif
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef int64_t  TUID ;
-typedef uint64_t SUID ;
-typedef int32_t  CUID ;
-typedef uint64_t FUID ;
-
-// Create SUID number
-UUIDs_EXPORT SUID   randomUpperSuid (void) ;
-UUIDs_EXPORT SUID   randomUpperSuid (SUID header) ;
-UUIDs_EXPORT SUID   randomLongSuid  (void) ;
-UUIDs_EXPORT SUID   randomLongSuid  (SUID tailer) ;
-UUIDs_EXPORT SUID   randomSuid      (SUID extension=0,bool upper=true) ;
-UUIDs_EXPORT TUID   randomTuid      (void) ;
-UUIDs_EXPORT TUID   randomTuid      (SUID header) ;
-
-// String operation
-UUIDs_EXPORT SUID   suidFromHex     (const char * string,size_t len=0) ;
-UUIDs_EXPORT TUID   tuidFromHex     (const char * string,size_t len=0) ;
-
-// Create SUID Memory
-UUIDs_EXPORT void * suidArray       (size_t size) ;
-UUIDs_EXPORT void * suidResize      (void * block,size_t size) ;
-UUIDs_EXPORT void   suidFree        (void * block) ;
-
-// Sorting
-UUIDs_EXPORT void   suidSort        (void * block,size_t size) ;
-UUIDs_EXPORT void   tuidSort        (void * block,size_t size) ;
-
-// Uniqueness
-UUIDs_EXPORT size_t suidUnique      (void * unique,const void * block,size_t size) ;
-UUIDs_EXPORT size_t tuidUnique      (void * unique,const void * block,size_t size) ;
-
-// Reversal
-UUIDs_EXPORT void   suidReverse     (void * reverse,const void * block,size_t size) ;
-UUIDs_EXPORT void   tuidReverse     (void * reverse,const void * block,size_t size) ;
-
-/****************************************************************************\
- *                                                                          *
- *  Destroyer is a class that helps to destroy any classes which does not   *
- *  know when will it is about to be destroyed.                             *
- *                                                                          *
-\****************************************************************************/
-
-class UUIDs_EXPORT Destroyer
+class BmpObject
 {
   public:
 
-    explicit     Destroyer  (void) ;
-    virtual     ~Destroyer  (void) ;
+    explicit         BmpObject (void) ;
+    virtual         ~BmpObject (void) ;
 
-    virtual bool Recycling  (void) ;
-    virtual bool Destructor (void) ;
+    virtual void     setData   (void * data,uint32_t size) ;
+    virtual void   * data      (void) ;
+    virtual uint32_t size      (void) ;
 
   protected:
+
+    void *   bmpData  ;
+    uint32_t dataSize ;
 
   private:
 
